3a16a87f29f949c7297361a35d7b7b028d67fc01
Faruk Efe Yencilek <eyencilek@lcs.on.ca>
2021-09-16 21:56:42 -0400

    Initial Commit

---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by Efe Yencilek on 2021-09-16.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
